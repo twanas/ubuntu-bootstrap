0c79ed3e97f0e4d3ac40fc8b9d64c0b9e035b409
antoine waugh <42454143+twanas@users.noreply.github.com>
2020-03-23 16:03:51 +1100

    Update zerotier.sh

--- core/zerotier.sh
+++ core/zerotier.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+echo "[ Zero-tier setup ]"
+echo ""
+echo "Install zero-tier vpn client? (y/n): "
+read install_zerotier
+
+if [[ $install_zerotier == "y" || $install_zerotier == "Y" || $install_zerotier == "yes" ]]; then
+    echo "** Installing Zerotier VPN Client **"
 curl -s https://install.zerotier.com | sudo bash
+fi
