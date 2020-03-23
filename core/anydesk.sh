764fef17b2c6f6e517022ccba9cfffcef17c9023
antoine waugh <42454143+twanas@users.noreply.github.com>
2020-03-23 14:23:01 +1100

    Update anydesk.sh

--- core/anydesk.sh
+++ core/anydesk.sh
@@ -2,11 +2,11 @@
 
 # download apt-key
 
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
 
 # add the repository:
 
-echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
 
 # update apt cache
 
